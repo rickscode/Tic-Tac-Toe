0d915dcd3a1e34682adc6c59432a669ea7f70f41
“Ricky <grickyh@gmail.com>
2021-03-19 17:06:02 +0000

    displaying gameBoard array as html object

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 // if one thing use modules e.g gameboard or displayController
 // if need multiples e.g players use factory functions
 
+
+
 const gameBoard = (() => {
     let board = [1,2,3,4,5,6,7,8,9];
-    return board;
+    board.forEach(displayGame);
+    
+
+    
+function displayGame(item) {
+    const boardContainer = document.querySelector(".board-container");
+    
+    
+    
+    let gameSquare = document.createElement("div");
+    gameSquare.textContent = item;
+    boardContainer.appendChild(gameSquare);
+    
+
+    
+}
+
 })();
+
 
 
