992bf37ada8cb4bf9d8bc9d13c45510072b20fcc
“Ricky <grickyh@gmail.com>
2021-03-20 08:39:06 +0000

    player x turn function

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // if need multiples e.g players use factory functions
 
 
-
+// gameBoard module iife
 const gameBoard = (() => {
     let board = [1,2,3,4,5,6,7,8,9];
     board.forEach(displayGame);
@@ -15,6 +15,7 @@ function displayGame(item) {
     
     
     let gameSquare = document.createElement("div");
+    gameSquare.id = "square";
     gameSquare.textContent = item;
     boardContainer.appendChild(gameSquare);
     
@@ -24,5 +25,26 @@ function displayGame(item) {
 
 })();
 
+// player X turn function
+const playerX = () => {
+    let value = "X";
+    chooseSquare();
 
+    // gameboard square selection function
+    function chooseSquare() {
+        const boardContainer = document.getElementById("square");
+        boardContainer.addEventListener("click", placeX);
+        // place X on chosen square   
+        function placeX(e){
+            if(e.target.id === "square"){
+                e.target.textContent = "X";
+            }
+        }
+    }
+    
+    
+
+}
+
+playerX();
 
