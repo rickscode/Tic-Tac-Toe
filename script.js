c730b068142337dcf577913cf3e6afa30565922e
“Ricky <grickyh@gmail.com>
2021-03-25 07:13:00 +0000

    added reset board function

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const gameBoard = (() => {
     const boardArray = [1,2,3,4,5,6,7,8,9];
 
     createGameBoard();
-    // needs to just be create board function
+    
 function createGameBoard() {
     const changeBoardArrayValue = document.querySelector(".board-container");
     
@@ -12,63 +12,63 @@ function createGameBoard() {
         if(boardArray[m] == 1){
             let gameSquare = document.createElement("div");
             gameSquare.id = "square-one";
-            
+            gameSquare.className = "game-square"
             changeBoardArrayValue.appendChild(gameSquare);
             
         } else if(boardArray[m] == 2){
             let gameSquare = document.createElement("div");
             gameSquare.id = "square-two";
-            
+            gameSquare.className = "game-square"
             changeBoardArrayValue.appendChild(gameSquare);
             
         } else if(boardArray[m] == 3){
             let gameSquare = document.createElement("div");
             gameSquare.id = "square-three";
-            
+            gameSquare.className = "game-square"
             changeBoardArrayValue.appendChild(gameSquare);
             
         } else if(boardArray[m] == 4){
             let gameSquare = document.createElement("div");
             gameSquare.id = "square-four";
-            
+            gameSquare.className = "game-square"
             changeBoardArrayValue.appendChild(gameSquare);
             
         } else if(boardArray[m] == 5){
             let gameSquare = document.createElement("div");
             gameSquare.id = "square-five";
-            
+            gameSquare.className = "game-square"
             changeBoardArrayValue.appendChild(gameSquare);
            
             
         } else if(boardArray[m] == 6){
             let gameSquare = document.createElement("div");
             gameSquare.id = "square-six";
-            
+            gameSquare.className = "game-square"
             changeBoardArrayValue.appendChild(gameSquare);
             
         } else if(boardArray[m] == 7){
             let gameSquare = document.createElement("div");
             gameSquare.id = "square-seven";
-            
+            gameSquare.className = "game-square"
             changeBoardArrayValue.appendChild(gameSquare);
             
         } else if(boardArray[m] == 8){
             let gameSquare = document.createElement("div");
             gameSquare.id = "square-eight";
-            
+            gameSquare.className = "game-square"
             changeBoardArrayValue.appendChild(gameSquare);
             
         } else if(boardArray[m] == 9){
             let gameSquare = document.createElement("div");
             gameSquare.id = "square-nine";
-            
+            gameSquare.className = "game-square"
             changeBoardArrayValue.appendChild(gameSquare);
             
         }
-         
-    } 
-
-  }
+        
+      } 
+    
+    }
 
 return {boardArray};
 
@@ -105,10 +105,12 @@ changeBoardArrayValue.addEventListener("click", placeMarker);
 
 function placeMarker(e) {
   
+  checkTurn();
+
   switch(e.target.id) {
        case "square-one":
         if(gameBoard.boardArray[0] == 1){
-          checkTurn();
+          
           gameBoard.boardArray[0] = playerTurn;
           e.target.textContent = gameBoard.boardArray[0]
           console.log(gameBoard.boardArray)
@@ -116,7 +118,7 @@ function placeMarker(e) {
          break;
        case "square-two":
         if(gameBoard.boardArray[1] == 2){
-          checkTurn();
+          
           gameBoard.boardArray[1] = playerTurn;
           e.target.textContent = gameBoard.boardArray[1]
           console.log(gameBoard.boardArray)
@@ -124,7 +126,7 @@ function placeMarker(e) {
          break;
        case "square-three":
         if(gameBoard.boardArray[2] == 3){
-          checkTurn();
+          
           gameBoard.boardArray[2] = playerTurn;
           e.target.textContent = gameBoard.boardArray[2]
           console.log(gameBoard.boardArray)
@@ -132,7 +134,7 @@ function placeMarker(e) {
          break;
        case "square-four":
         if(gameBoard.boardArray[3] == 4){
-          checkTurn();
+          
           gameBoard.boardArray[3] = playerTurn;
           e.target.textContent = gameBoard.boardArray[3]
           console.log(gameBoard.boardArray)
@@ -140,7 +142,6 @@ function placeMarker(e) {
          break;
        case "square-five":
         if(gameBoard.boardArray[4] == 5){
-          checkTurn();
           gameBoard.boardArray[4] = playerTurn;
           e.target.textContent = gameBoard.boardArray[4]
           console.log(gameBoard.boardArray)
@@ -148,7 +149,7 @@ function placeMarker(e) {
          break;
        case "square-six":
         if(gameBoard.boardArray[5] == 6){
-          checkTurn();
+          
           gameBoard.boardArray[5] = playerTurn;
           e.target.textContent = gameBoard.boardArray[5]
           console.log(gameBoard.boardArray)
@@ -156,7 +157,7 @@ function placeMarker(e) {
          break;
        case "square-seven":
         if(gameBoard.boardArray[6] == 7){
-          checkTurn();
+          
           gameBoard.boardArray[6] = playerTurn;
           e.target.textContent = gameBoard.boardArray[6]
           console.log(gameBoard.boardArray)
@@ -164,7 +165,7 @@ function placeMarker(e) {
          break;
        case "square-eight":
         if(gameBoard.boardArray[7] == 8){
-          checkTurn();
+          
           gameBoard.boardArray[7] = playerTurn;
           e.target.textContent = gameBoard.boardArray[7]
           console.log(gameBoard.boardArray)
@@ -172,7 +173,7 @@ function placeMarker(e) {
          break;
        case "square-nine":
         if(gameBoard.boardArray[8] == 9){
-          checkTurn();
+          
           gameBoard.boardArray[8] = playerTurn;
           e.target.textContent = gameBoard.boardArray[8]
           console.log(gameBoard.boardArray)
@@ -187,22 +188,38 @@ function placeMarker(e) {
     function checkGame() {
       // horizontal wins
       if(gameBoard.boardArray[0] == gameBoard.boardArray[1] && gameBoard.boardArray[1] == gameBoard.boardArray[2]){
-        alert("winner");
+        alert("winner"); 
+        resetGameBoard()      
       } else if(gameBoard.boardArray[3] == gameBoard.boardArray[4] && gameBoard.boardArray[4] == gameBoard.boardArray[5]){
         alert("winner");
+        resetGameBoard()
       } else if(gameBoard.boardArray[6] == gameBoard.boardArray[7] && gameBoard.boardArray[7] == gameBoard.boardArray[8]){
         alert("winner");
+        resetGameBoard()
       // vertical wins
       } else if(gameBoard.boardArray[0] == gameBoard.boardArray[3] && gameBoard.boardArray[3] == gameBoard.boardArray[6]){
         alert("winner");
+        resetGameBoard()
       } else if(gameBoard.boardArray[1] == gameBoard.boardArray[4] && gameBoard.boardArray[4] == gameBoard.boardArray[7]){
         alert("winner");
+        resetGameBoard()
       } else if(gameBoard.boardArray[2] == gameBoard.boardArray[5] && gameBoard.boardArray[5] == gameBoard.boardArray[8]){
         alert("winner");
+        resetGameBoard()
         // diaganol wins
       } else if(gameBoard.boardArray[0] == gameBoard.boardArray[4] && gameBoard.boardArray[4] == gameBoard.boardArray[8]){
         alert("winner");
+        resetGameBoard()
       } else if(gameBoard.boardArray[2] == gameBoard.boardArray[4] && gameBoard.boardArray[4] == gameBoard.boardArray[6]){
         alert("winner");
-      }
+        resetGameBoard()
+      } 
+    }
+
+    function resetGameBoard() {
+      gameBoard.boardArray = [1,2,3,4,5,6,7,8,9];
+      let newGameBoard = document.querySelectorAll(".game-square")
+      newGameBoard.forEach(item => {
+        item.textContent = ""
+      });
     }
